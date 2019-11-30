d3ea5d106f2da6af29a26b8fcbb6b0859a420b02
Laki Sik <laki@th.ibm.com>
2019-11-30 10:11:49 +0700

    git cache cleared

--- docker-compose/postgre/init.sql
+++ docker-compose/postgre/init.sql
@@ -8,6 +8,7 @@ DROP USER IF EXISTS "gitlab";
 CREATE USER "gitlab" with encrypted password 'P@ssw0rd';
 CREATE DATABASE "gitlabhq_production" WITH OWNER "gitlab" ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8' TEMPLATE template0;
 GRANT ALL PRIVILEGES ON DATABASE gitlabhq_production to gitlab;
+CREATE EXTENSION pg_trgm;
 
 
 DROP USER IF EXISTS "sonarqube";
--- kubernetes/kops/setup-cluster.sh
+++ kubernetes/kops/setup-cluster.sh
@@ -12,9 +12,22 @@ http://www.studytrails.com/devops/kubernetes/install-kubernetes-on-aws-using-kop
 https://kubernetes.io/docs/setup/production-environment/tools/kops/
 
 DNS
-https://check-host.net/check-dns?host=api.ap1.kub.tripspace.co
+https://check-host.net/check-dns?host=api.apsoutheast.kub.trips2thailand.com
 
 
 k get nodes --output=wide
 
 kubectl expose deployment deploy-1 --type=LoadBalancer -n=my-service
+
+
+// route53
+ID=$(uuidgen) && aws route53 create-hosted-zone --name kub.trips2thailand.com --caller-reference $ID | jq .DelegationSet.NameServers
+aws route53 list-hosted-zones | jq '.HostedZones[] | select(.Name=="trips2thailand.com.") | .Id'
+aws route53 change-resource-record-sets --hosted-zone-id /hostedzone/Z3LBIFLO5YT25R --change-batch file://kub.dns.json
+
+Suggestions:
+ * validate cluster: kops validate cluster
+ * list nodes: kubectl get nodes --show-labels
+ * ssh to the master: ssh -i ~/.ssh/id_rsa admin@api.apsoutheast.kub.trips2thailand.com
+ * the admin user is specific to Debian. If not using Debian please use the appropriate user based on your OS.
+ * read about installing addons at: https://github.com/kubernetes/kops/blob/master/docs/addons.md.
